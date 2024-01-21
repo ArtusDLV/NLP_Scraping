b08ebedc2a81fcbaceab6484784a4608c79dd6db
Thomas BOUGUET <t.bouguet@orange.fr>
2024-01-21 15:51:39 +0100

    First simple model

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -8,10 +8,12 @@ import pandas as pd
 
 import re
 from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
+# from nltk.stem import PorterStemmer
 from spacy import load as Lemmatizer
 from nltk.corpus import stopwords
 # from nltk.data import load
+
+from sklearn.model_selection import train_test_split
 
 
 ## Variables ##
@@ -23,21 +25,24 @@ link_raw_data = 'data/trustpilot_en_50_page.csv'
 ## Functions ##
 
 
-def clean_df(df=pd.read_csv(link_raw_data)):
+def clean_df(column='Review',df=pd.read_csv(link_raw_data)):
 
-    df_clean = df.drop('Unnamed: 0',axis=1)
-    df_clean = df_clean.dropna(subset=['Review']).reset_index(drop=True)
+    if 'Unnamed: 0' in df.columns:
+        df_clean = df.drop('Unnamed: 0',axis=1)
+    else:
+        df_clean = df.copy()
+    df_clean = df_clean.dropna(subset=[column]).reset_index(drop=True)
 
     return df_clean
 
-def tokenize(corpus):
+def tokenize(corpus,column='Review'):
 
     if type(corpus) == pd.core.frame.DataFrame:
-        corpus = corpus['Review'].to_list()
+        corpus = corpus[column].to_list()
 
     regex = re.compile('([^\s\w]|_)+')
     stops = set(stopwords.words('english'))
-    stemmer_output = PorterStemmer()
+    # stemmer_output = PorterStemmer()
     lemmatizer_output = Lemmatizer("en_core_web_sm")
 
     new_corpus = []
@@ -57,7 +62,11 @@ def load_data():
 
     df = clean_df()
     corpus = tokenize(df)
-    df['Tokenized_reviews'] = corpus
+    df['Tokenized_review'] = corpus
 
     return df
+
+def train_test(df,seed=42):
+
+    return train_test_split(df['Tokenized_headline'], df['Tokenized_review'], test_size=0.33, random_state=seed)
 
